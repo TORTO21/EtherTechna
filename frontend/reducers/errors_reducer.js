5814db9b8a6387c6934f695697fc31a46a21cf1d
Oliver Almalel <oliver.almalel@gmail.com>
2019-05-22 19:32:19 -0700

    | added slice of errors reducer | added errors session actions, reducer | fixed spacing for modal close icon |

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import sessionErrorsReducers from './session_errors_reducer'
+
+const errorsReducer = combineReducers({
+  session: sessionErrorsReducers
+})
+
+export default errorsReducer
